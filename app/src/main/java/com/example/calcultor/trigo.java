b8df29cfacef29277561d9e5065dd239f25ca709
naman-290 <66725877+naman-290@users.noreply.github.com>
2020-10-07 15:04:04 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/calcultor/trigo.java
@@ -0,0 +1,14 @@
+package com.example.calcultor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class trigo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trigo);
+    }
+}
